6e889eeaeb7cf5b99b238d82b67e53ef97777488
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-11 17:54:40 +0000

    completed second task

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,17 @@
-console.log("Hello World!");
+// First program
 
-setTimeout(function() {
-        console.log("Hello World Again!!");
-    }, 10000);
+// console.log("Hello World!");
+
+// setTimeout(function() {
+//         console.log("Hello World Again!!");
+//     }, 10000);
+
+
+//A wild interval has appeared!
+
+function timeFunc(){
+        console.log('Hello World!');
+        setTimeout(timeFunc, 10000);
+}
+
+timeFunc();
